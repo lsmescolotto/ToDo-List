9d5e7bb49d83a698775d41dd06dab6b736931b62
Luiza Schmidt Mescolotto <lsmescolotto@gmail.com>
2023-03-24 21:29:55 -0300

    feat: create handleCompleteAndUncompometeTask and handleDeleteTask functions

--- src/components/TaskCardItem.tsx
+++ src/components/TaskCardItem.tsx
@@ -1,33 +1,57 @@
 import styles from "./TaskCardItem.module.css";
 import { Trash } from "phosphor-react";
 import { Check } from "phosphor-react";
+import { Dispatch, SetStateAction } from "react";
+import { Task } from "../App";
 
 interface TaskCardItemProps {
-  task: { id: string; name: string; completed: boolean };
+  task: Task;
+  tasksList: Task[];
+  setTasksList: Dispatch<SetStateAction<Task[]>>;
 }
 
-const TaskCardItem = (props: TaskCardItemProps) => {
-  const { task } = props;
+export const TaskCardItem = (props: TaskCardItemProps) => {
+  const { task, tasksList, setTasksList } = props;
   const { completed, name, id } = task;
+
+  const handleCompleteAndUncompleteTask = () => {
+    const indexOfTask = tasksList.findIndex((taskItem) => taskItem.id === id);
+
+    const tasksListCopy = [...tasksList];
+
+    tasksListCopy.splice(indexOfTask, 1, {
+      ...task,
+      completed: !completed,
+    });
+
+    setTasksList(tasksListCopy);
+  };
+
+  const handleDeleteTask = () => {
+    const newTasksList = tasksList.filter((taskItem) => taskItem.id !== id);
+    setTasksList(newTasksList);
+  };
 
   return (
     <div className={styles.card}>
-      {completed ? (
-        <div className={styles.checked} title="Mark as not completed">
-          <Check />
-        </div>
-      ) : (
-        <div className={styles.checkbox} title="Complete task" />
-      )}
+      <div
+        className={completed ? styles.checked : styles.checkbox}
+        title={completed ? "Mark as not completed" : "Complete task"}
+        onClick={handleCompleteAndUncompleteTask}
+      >
+        {completed && <Check />}
+      </div>
 
       <p className={completed ? styles.completedTaskText : styles.taskText}>
         {name}
       </p>
-      <button title="Delete task" className={styles.deleteTask}>
+      <button
+        title="Delete task"
+        className={styles.deleteTask}
+        onClick={handleDeleteTask}
+      >
         <Trash size={16} />
       </button>
     </div>
   );
 };
-
-export default TaskCardItem;
